ba9b0ff04aa9102a92900b0d8b8727114cccf2d1
TurkevychCode <vitalik0306t@gmail.com>
2024-10-31 02:41:56 +0200

    img solving

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -20,22 +20,22 @@ function Navbar() {
             href="https://www.linkedin.com/in/vitalii-turkevych-60b5281ab/"
             target="_blank"
           >
-            <img src="/linkedin.png" alt="linkedIn" />
+            <img src="/public/linkedin.png" alt="linkedIn" />
           </a>
           <a
             href="https://www.instagram.com/vitaliik.st/profilecard/?igsh=a25tZnhxbGIwNWt3"
             target="_blank"
           >
-            <img src="/instagram.png" alt="Ins" />
+            <img src="/public/instagram.png" alt="Ins" />
           </a>
           <a href="https://github.com/TurkevychCode" target="_blank">
-            <img src="/github.png" alt="Github" />
+            <img src="/public/github.png" alt="Github" />
           </a>
           <a
             href="https://www.facebook.com/vitalik.turkevych.3/"
             target="_blank"
           >
-            <img src="/portfolio/public/facebook.png" alt="facebook" />
+            <img src="/public/facebook.png" alt="facebook" />
           </a>
         </div>
       </div>
