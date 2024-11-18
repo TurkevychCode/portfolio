b74483fa36dfa1c72b778696fe0477a686a4c0e5
TurkevychCode <vitalik0306t@gmail.com>
2024-11-18 14:13:05 +0200

    delete inst icon

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -22,20 +22,8 @@ function Navbar() {
           >
             <img src="./linkedin.png" alt="linkedIn" />
           </a>
-          <a
-            href="https://www.instagram.com/vitaliik.st/profilecard/?igsh=a25tZnhxbGIwNWt3"
-            target="_blank"
-          >
-            <img src="./instagram.png" alt="Ins" />
-          </a>
           <a href="https://github.com/TurkevychCode" target="_blank">
             <img src="./github.png" alt="Github" />
-          </a>
-          <a
-            href="https://www.facebook.com/vitalik.turkevych.3/"
-            target="_blank"
-          >
-            <img src="./facebook.png" alt="facebook" />
           </a>
         </div>
       </div>
